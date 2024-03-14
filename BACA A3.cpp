620fb1ef8674b33f432af79558a506dcdcb03ae8
Karol Bargieł <122556971+KariPL2@users.noreply.github.com>
2024-03-14 15:24:05 +0100

    Add files via upload
    Zadanie A3 – Złe Piksele
    UWAGA
    Zadanie to można wykonać jedynie w języku C++
    
    Zadanie
    Ada odziedziczyła po dziadku ogromny wyświetlacz LED do zainstalowania na budynku swojej firmy programistycznej. Niestety, wyświetlacz jest już leciwy i posiada wiele zepsutych pikseli (bad pixels). Ada chce się dowiedzieć jak duże logo jest w stanie przygotować, aby można je było w całości wyświetlić bez żadnych złych pikseli w jego obrębie. Nie będziemy wnikać jak wygląda logo firmy Ady, będzie dla nas ważne jedynie to, że jest ono kwadratem.
    
    Pomóż Adzie i dla zadanego opisu ekranu podaj jak duże logo może ona przygotować.
    
    Opis wejścia
    Program ma wczytywać dane ze standardowego wejścia.
    
    W pierwszej linii wejścia znajduje się jedna liczba 0 < Z <= 100 oznaczająca ilość zestawów danych do przetworzenia.
    W kolejnych liniach znajdują się zestawy danych.
    Pierwsza linia zestawu danych zawiera dwie liczby naturalne oddzielone pojedynczą spacją: H W (w tej kolejności), 1 < H, W <= 5000 oznaczające odpowiednio wysokość i długość wyświetlacza LED.
    W kolejnych H liniach znajduje się opis kolejnych linii pikseli wyświetlacza składający się z liter 'o' (oznaczający dobry piksel) i 'x' (oznaczający zły piksel). W każdej linii jest W symboli.
    Opis wyjścia
    Program ma wypisać rozwiązanie na standardowe wyjście. program ma wypisać dokładnie Z linii, po jednej dla każdego zestawu danych.
    
    Dla każdego zestawu danych program ma wypisać jedną liczbę w k-tej linii - maksymalną długość boku dla loga Ady, która zmieści się na wyświetlaczu i nie zahaczy o żaden zły piksel w zestawie danych k.
    
    Obostrzenia
    Plagiaty będą rozpatrywane na koniec semestru.
    Przykładowe wejście
    Dla danych:
    
    1
    4 7
    ooooooo
    ooooooo
    oooxooo
    ooooooo
    Poprawnym wyjściem jest:
    
    3

---
+++ BACA A3.cpp
@@ -0,0 +1,79 @@
+﻿//Karol Bargiel
+
+#include <iostream>
+using namespace std;
+#include <vector>
+
+// Funkcja obliczająca rozmiar maksymalnego kwadratu złożonego z '1' w macierzy binarnej.
+int maximalSquare(vector<vector<char> >& matrix) {
+    int row = matrix.size(); // Pobranie liczby wierszy w macierzy.
+    int col = matrix[0].size(); // Pobranie liczby kolumn w macierzy.
+    vector<int> prev(col); // Wektor przechowujący poprzedni wiersz.
+    vector<int> curr(col); // Wektor przechowujący bieżący wiersz.
+    int maxi = 0; // Zmienna przechowująca maksymalny rozmiar kwadratu.
+
+    // Inicjalizacja pierwszego wiersza i obliczanie maksymalnego rozmiaru.
+    for (int j = 0; j < col; j++) {
+        prev[j] = matrix[0][j] - '0';
+    // Operator || (logical OR) wykonuje się w taki sposób, że jeśli jedna z wartości jest true,
+    // to wynik będzie true. Działa to jako sprawdzenie, czy w pierwszym wierszu jest co najmniej jedno '1'.
+        maxi = maxi || prev[j];
+    }
+
+    // Przechodzenie przez pozostałe wiersze macierzy.
+    for (int i = 1; i < row; i++) {
+        curr[0] = matrix[i][0] - '0';
+        maxi = max(maxi, curr[0]);
+
+        // Przechodzenie przez pozostałe kolumny w bieżącym wierszu.
+        for (int j = 1; j < col; j++) {
+            // Obliczanie długości boku maksymalnego kwadratu w aktualnym punkcie.
+            if (matrix[i][j] == '1')
+                curr[j] = 1 + min(min(curr[j - 1], prev[j - 1]), prev[j]);
+            else
+                curr[j] = 0;
+
+            // Aktualizacja maksymalnego rozmiaru kwadratu.
+            maxi = max(maxi, curr[j]);
+        }
+
+        // Przesunięcie bieżącego wiersza do poprzedniego wiersza.
+        prev = curr;
+    }
+
+    return maxi; // Zwrócenie maksymalnego rozmiaru kwadratu.
+}
+
+int main() {
+    int Z; // Liczba zestawów danych.
+    cin >> Z;
+
+    // Przechodzenie przez wszystkie zestawy danych.
+    for (int k = 0; k < Z; k++) {
+        int H, W; // Wysokość i szerokość macierzy.
+        cin >> H >> W;
+        vector<vector<char> > tab(H, vector<char>(W)); // Inicjalizacja macierzy.
+
+        // Wczytywanie wartości do macierzy.
+        for (int i = 0; i < H; i++) {
+            for (int j = 0; j < W; j++) {
+                char znak;
+                cin >> znak;
+
+                // Konwersja znaku 'o' na '1' i pozostałych znaków na '0'.
+                if (znak == 'o') {
+                    tab[i][j] = '1';
+                }
+                else {
+                    tab[i][j] = '0';
+                }
+            }
+        }
+
+        // Wywołanie funkcji obliczającej maksymalny rozmiar kwadratu i wypisanie wyniku.
+        int result = maximalSquare(tab);
+        cout << result << '\n';
+    }
+
+    return 0;
+}
