2ba6ef75ef9444a3879a2137015cd2640e1d3e12
Karol Bargieł <122556971+KariPL2@users.noreply.github.com>
2024-03-14 15:18:58 +0100

    Add files via upload
    Zadanie A2 – Bal
    UWAGA
    Zadanie to można wykonać jedynie w języku C++
    
    Zadanie
    Na coroczny Bal Charytatywny do króla Bajtazara przybyli arystokraci z wszystkich stron królestwa. Ponieważ pogoda w tym roku nie rozpieszcza więc, aby oszczędzić arytokratom niewygód, postanowiono wpuścić ich do pałacu wszystkimi dostępnymi wejściami - po jednym w każdym skrzydle zamku. Niestety, protokół dyplomatyczny wymaga, aby na salę w pierwszej kolejności wchodzili arystokraci najmniej ważni - o najmniejszym prestiżu. Dzięki czemu osoby w królestwie najważniejsze zostaną przedstawione największej liczbie osób. Na szczęście, arystokracja dobrze zna ten obyczaj i w każdym skrzydle samoistnie ustawiła się według rosnącego prestiżu.
    
    Na Ciebie, jako Nadwornego Heralda, spada teraz zadanie wywołania kolejnych arystokratów. Pamiętaj, że w każdym korytarzu prowadzącym z każdego skrzydła widzisz tylko pierwszego arystokratę (o najmniejszym prestiżu w danym korytarzu). Postaraj sie wykonać powierzone zadanie jak najszybciej, gdyż zarówno Król jak i arystokraci nie lubią opieszałości, a najważniejsi goście czekają przecież na końcach kolejek!
    
    Pamietaj także, żeby wywołując gości nie ogłosić ich prestiżu, gdyż nie chcemy, żeby ktoś poczuł się zawstydzony publicznym ogłoszeniem jego niskiego statusu społecznego - to wywołałoby skandal i kryzys dyplomatyczny!
    
    Opis wejścia
    Program ma wczytywać dane ze standardowego wejścia.
    
    W pierwszej linii wejścia znajduje się jedna liczba naturalna 0 < M <= 10^6 oznaczająca ilość skrzydeł w zamku (i jednoczesnie ilość korytarzy w których czekają arystokraci). W kolejnych liniach znajdują się opisy arystokratów czekających w każdym z korytarzy, ułożone według rosnącego prestiżu.
    Korytarze numerowane są kolejnymi liczbami naturalnymi (od 1). Opis korytarza i-tego znajduje się w linii i+1.
    Opis i-tego korytarza zaczyna się od liczby naturalnej 0 < Ni <= 10^6 oznaczającej ilość arystokratów czekających w tym korytarzu. Po tej liczbie wystepuje pojedyncza spacja i dalej znajduje się Ni par, oddzielonych pojedynczą spacją, opisujących kolejnych gości czekających w korytarzu.
    Każdy gość opisany jest parą p s (elementy pary oddzielone spacją), gdzie -231 <= p <= 231-1 oznacza prestiż osoby (osoby o szemranej reputacji moga mieć prestiż ujemny!), natomiast s jest nazwiskiem osoby, składającym się z liter alfabetu angielskiego, kropki (.) lub pauzy (-).
    Sumaryczna liczba gości Balu nie przekroczy 107, natomiast nazwisko żadnej z osób nie przekroczy 31 znaków.
    W każdym korytarzu osoby podane na wejściu są w kierunku rosnącego prestiżu.
    Opis wyjścia
    Program ma wypisać rozwiązanie na standardowe wyjście.
    
    Program ma wypisać nazwiska wszystkich osób w kolejności ich wywoływania. Każde nazwisko należy wypisać w osobnej linii, w kierunku rosnącego prestiżu (najbardziej prestiżowa osoba zostanie przedstawiona wszystkim).
    
    Jeśli dwie osoby mają taki sam prestiż w kilku różnych korytarzach, to wywoływana jest osoba z korytarza, w którym czeka najwięcej osób. Jeśli jest kilka takich osób, to wybieramy osobę z korytarza o numerze najmniejszym.
    
    Obostrzenia
    Nie wolno używać wbudowanych (std::, itp.) algorytmów.
    Nie wolno używać żadnych kontenerów poza podanymi kontenerami tablicowymi: std::vector.
    Wolno używać klasy typu std::string do przechowywania nazwisk.
    Plagiaty będą rozpatrywane na koniec semestru, tak samo jak spełnienie warunków nie wolno.
    Przykładowe wejście
    Dla danych:
    
    3
    2 99 Ser.Pounce-a-lot 100 Arya
    3 -100 Joker 0 Maleficent 99 Harry
    4 -50 Bedevere 2 Arthur 3 Bryan 100 Tim
    Poprawnym wyjściem jest:
    
    Joker
    Bedevere
    Maleficent
    Arthur
    Bryan
    Ser.Pounce-a-lot
    Harry
    Arya
    Tim

---
+++ BACA A2.cpp
@@ -0,0 +1,178 @@
+﻿//Karol Bargiel
+#include <iostream>
+class Aristocrat;
+class Aristocrat {
+public:
+	int priority;
+	std::string name;
+	int number_corridor;
+	int length;
+	int curr_ind;
+	Aristocrat() {};
+	Aristocrat(int priority, std::string name, int number_corridor, int length, int curr_ind) {
+		this->priority = priority;
+		this->name = name;
+		this->number_corridor = number_corridor;
+		this->length = length;
+		this->curr_ind = curr_ind;
+	}
+
+};
+class Corridor {
+public:
+	int length;
+	Aristocrat* A;
+	Corridor() {};
+	Corridor(int length) {
+		this->length = length;
+		A = new Aristocrat[length];
+	}
+
+
+};
+
+
+
+class queueArray {
+public:
+	int maxSize;
+	Aristocrat* elem;
+	int Size;
+
+	queueArray(int size) {
+		maxSize = size;
+		elem = new Aristocrat[maxSize];
+		Size = 0;
+	}
+
+	void enQueue(Aristocrat x) {
+		if (isFull()) {
+			return;
+		}
+		else {
+			elem[Size] = x;
+			heapifyUp(Size);
+			Size++;
+		}
+	}
+
+	Aristocrat deQueue() {
+		if (isEmpty()) {
+			return Aristocrat();
+		}
+
+		Aristocrat tmp = elem[0];
+		elem[0] = elem[Size - 1];
+		Size--;
+		heapifyDown(0);
+
+		return tmp;
+	}
+
+	Aristocrat getFront() {
+		if (isEmpty()) {
+			return Aristocrat();
+		}
+		else {
+			return elem[0];
+		}
+	}
+
+	bool isEmpty() {
+		return (Size == 0);
+	}
+
+	bool isFull() {
+		return (Size == maxSize);
+	}
+
+	void heapifyUp(int index) {
+		while (index > 0) {
+			int parentIndex = (index - 1) / 2;
+			if (compare(elem[index], elem[parentIndex])) {
+				swap(elem[index], elem[parentIndex]);
+				index = parentIndex;
+			}
+			else {
+				break;
+			}
+		}
+	}
+
+	void heapifyDown(int index) {
+		int leftChild = 2 * index + 1;
+		int rightChild = 2 * index + 2;
+		int smallest = index;
+
+		if (leftChild < Size && compare(elem[leftChild], elem[smallest])) {
+			smallest = leftChild;
+		}
+		if (rightChild < Size && compare(elem[rightChild], elem[smallest])) {
+			smallest = rightChild;
+		}
+
+		if (smallest != index) {
+			swap(elem[index], elem[smallest]);
+			heapifyDown(smallest);
+		}
+	}
+
+	bool compare(Aristocrat& a, Aristocrat& b) {
+		return (a.priority < b.priority) ||
+			(a.priority == b.priority && (a.length - a.curr_ind) > (b.length - b.curr_ind)) ||
+			(a.priority == b.priority && (a.length - a.curr_ind) == (b.length - b.curr_ind) && a.number_corridor < b.number_corridor);
+	}
+
+	void swap(Aristocrat& a, Aristocrat& b) {
+		Aristocrat temp = a;
+		a = b;
+		b = temp;
+	}
+};
+
+
+
+
+int main()
+{
+	int M; 
+	std::cin >> M;
+
+	queueArray q = queueArray(M);
+	Corridor** all_corridors = new Corridor * [M];
+	int priority;
+	std::string name;
+	for (int i = 0; i < M; i++)
+	{
+		int n; 
+		std::cin >> n;
+		Corridor* corridor = new Corridor(n);
+		all_corridors[i] = corridor;
+		for (int j = 0; j < n; j++)
+		{
+			std::cin >> priority >> name;
+			Aristocrat aristocrat(priority, name, i, n, j);
+			corridor->A[j] = aristocrat;
+		}
+
+		q.enQueue(corridor->A[0]);
+	}
+
+	while (!q.isEmpty())
+	{
+		Aristocrat currentPerson = q.getFront();
+		int currentCorridor = currentPerson.number_corridor;
+
+		std::cout << currentPerson.name << '\n';
+
+		q.deQueue();
+
+		int nextIndex = currentPerson.curr_ind + 1;
+		if (nextIndex < all_corridors[currentPerson.number_corridor]->length)
+		{
+			q.enQueue(all_corridors[currentPerson.number_corridor]->A[nextIndex]);
+		}
+	}
+
+	return 0;
+}
