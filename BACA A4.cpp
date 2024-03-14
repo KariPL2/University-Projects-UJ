1c89b29ea4256bd269d50ed434bf4be97c4cf0df
Karol Bargieł <122556971+KariPL2@users.noreply.github.com>
2024-03-14 15:20:44 +0100

    Add files via upload
    Zadanie A4 – Driving Home For Christmas
    UWAGA - Zadanie z GWIAZDKĄ (pun not intended)
    Zadanie można rozwiązać jedynie w terminie 22.12.2023 g20:00 - 06.01.2024 g 23:59:59, poza tym czasem nie będzie mozna wysyłać tego zadania.
    
    Zadanie ma wartość punktową 0 (nie liczy się na potrzeby BACAw - wynik ćwiczeń). Natomiast:
    
    Jego zrobienie na status zielony daje plus z aktywności (limit 5 plusów dalej obowiązuje).
    Jego zrobienie na status zielony daje możliwość zastąpienia jednego zadania bez statusu zielonego podczas wyliczania parametru BACAp
    Zadanie to można wykonać jedynie w języku C++
    
    Zadanie
    Chris jedzie na święta do domu i chce przesłuchać swój ulubiony audiobook - Opowieść Wigilijną. Audiobook trwa dokładnie L godzin. Chris chce się dowiedzieć, na ile różnych sposobów może wrócić do domu na święta, aby mógł spokojnie przesłuchać audiobooka w całości (ale żeby nie zużyć więcej czasu niż potrzeba). Mapa Chrisa składa się z N miast (numerowanych od 0 do N-1) połączonych drogami - jednokierunkowymi - ale mogą istnieć drogi powrotne. Nie ma natomiast dróg-obwodnic - łączących miasto z nim samym. Żadne dwa miasta nie są połączone bezpośrednio więcej niż jedną drogą w każdym kierunku. Podróż pomiędzy każdą połączoną drogą parą miast trwa dokładnie jedną godzinę. Miasto startowe ma numer 0 natomiast miasto docelowe ma numer N-1.
    
    Opis wejścia
    Program ma wczytywać dane ze standardowego wejścia.
    
    W pierwszej linii znajduje się jedna liczba naturalna 0 < Z <=100 oznaczająca ilość zestawów danych. W kolejnych liniach znajdują się zestawy danych.
    Zestaw k-ty danych składa się z Nk+1 linii.
    W pierwszej linii zestawu danych znajdują się trzy liczby oddzielone pojedynczą spacją: Nk Lk Mk 0 < Nk <= 100 - ilość miast, 0 <= Lk <= 264 - 1 - długość audiobooka, 0 < Mk <= 232 - 1 - specjalny parametr opisany dalej.
    W kolejnych Nk liniach znajdują się opisy połączeń miast. W linii pierwszej znajdują się połączenia z miasta 0, w drugiej z miasta 1 i tak dalej.
    Linia m-ta opisu połączeń (odpowiadająca miastu o numerze m-1) składa się z jednej liczby 0 <= cm <= Nk oznaczającej ilośc połączeń wychodzących z miasta, po czym po spacji występuje cm rożnych liczb z zakresu 0,...,Nk-1 (w dowolnej kolejności), oddzielonych spacją, oznaczających numery miast do których prowadzi droga z miasta m-1.
    Opis wyjścia
    Program ma wypisać rozwiązanie na standardowe wyjście. Program ma wypisać dokładnie Z linii, po jednej dla każdego zestawu danych.
    
    Dla każdego zestawu danych program ma wypisać jedną liczbę w k-tej linii, oznaczający ilość sposobów w jakie można odbyć podróż z miasta 0 do miasta Nk-1 (mod Mk).
    
    Obostrzenia
    Program ma składać się z jednego pliki .cpp.
    Plagiaty będą rozpatrywane na koniec semestru.
    Przykładowe wejście
    Dla danych:
    
    1
    3 3 1000
    2 1 2
    2 0 2
    2 0 1
    Poprawnym wyjściem jest:
    
    3
    Graf z zadania to:
    
    0 <-> 1
    ^     ^
    |     |
    v     |
    2 <---+
    A możliwe drogi długości 3:
    0-1-0-2
    0-2-0-2
    0-2-1-2

---
+++ BACA A4.cpp
@@ -0,0 +1,86 @@
+﻿// Karol Bargiel
+#include <iostream>
+#include <vector>
+std::vector<std::vector<unsigned long long> > adj_matrix;
+
+
+std::vector<std::vector<unsigned long long> > multiplyMatrices(std::vector<std::vector<unsigned long long> > a, std::vector<std::vector<unsigned long long> >& b, unsigned long long M) {
+    int N = a.size();
+    std::vector<std::vector<unsigned long long> > res(N, std::vector<unsigned long long>(N, 0));
+
+    for (int i = 0; i < N; i++)
+    {
+        for (int j = 0; j < N; j++)
+        {
+            for (int k = 0; k < N; k++) {
+                res[i][j] += (a[i][k] * b[k][j]) % M;
+            }
+            res[i][j] %= M;
+        }
+    }
+    return res;
+}
+
+
+
+std::vector<std::vector<unsigned long long> > matrixPowerL(unsigned long long L, std::vector<std::vector<unsigned long long> > mat, unsigned long long int M) {
+    if (L == 1) {
+        return mat;
+    }
+
+    std::vector<std::vector<unsigned long long> > half;
+    half = matrixPowerL(L / 2, mat, M);
+
+
+    if (L % 2 == 0) {
+        return multiplyMatrices(half, half, M);
+    }
+    else {
+        return multiplyMatrices(multiplyMatrices(half, half, M), mat, M);
+    }
+}
+
+
+
+int main() {
+    int Z;
+    std::cin >> Z;
+
+    long int N;
+    unsigned long long M;
+    unsigned long long L;
+
+    for (int i = 0; i < Z; ++i) {
+        
+        std::cin >> N >> L >> M;
+
+        
+
+        adj_matrix.resize(N, std::vector<unsigned long long>(N, 0));
+
+        for (int j = 0; j < N; ++j) {
+            int no_cities;
+            std::cin >> no_cities;
+            for (int k = 0; k < no_cities; ++k) {
+                int city;
+                std::cin >> city;
+                adj_matrix[j][city] = 1;
+            }
+        }
+
+        if (L != 0) {
+            std::vector<std::vector<unsigned long long> > res = matrixPowerL(L, adj_matrix, M);
+
+            std::cout << res[0][N - 1] << std::endl;
+            adj_matrix.clear();
+
+        }
+        else {
+            std::cout << 0 << std::endl;
+        }
+        //res.clear();
+        
+    }
+
+
+}
