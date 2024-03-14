7dc6895f9b49f56882d2855b090add7e8705f5b8
Karol Bargieł <122556971+KariPL2@users.noreply.github.com>
2024-03-14 15:21:38 +0100

    Add files via upload
    Zadanie A6 – Piksele Kontratakują
    UWAGA
    Zadanie to można wykonać jedynie w języku C++
    
    Zadanie
    Pomysłowa Ada postanowiła przekuć swoje doświadczenia we własny biznes i założyć startup zajmujący się bad-pixelami w ekranach komputerów. Niestety, ze względu na bliską Osobliwośc Technologiczną, wymiary ekranów stają się zastraszająco wielkie w bardzo krótkim czasie i ręczne ich sprawdzanie przestaje byc opłacalne. Ada stworzyła już skaner do złych pikseli, jednak potrzebuje oprogramowania, które pozwoli jej lepiej zrozumieć strukturę złych pikseli. Ada chce zacząć od czegoś prostego - policzenia, ile spójnych fragmentów matrycy jest pokrytych złymi pikselami, a dla każdego takiego spójnego pola - podać jeden z pikseli (aby łatwiej zlokalizować zły fragment w przyszłości).
    
    Dwa piksele nazywamy sąsiadującymi, jeśli leżą obok siebie na matrycy - ich współrzedne różnią się o 1 tylko na jednej współrzędnej. (Piksele nie siąsiadują po skosie). Zbiór złych pikseli w naturalny sposób nazywamy spójnym fragmentem, jeśli każde dwa piksele w tym zbiorze da się połaczyć ciągiem pikseli {pi}0 < i < k z tego zbioru, takim, że w ciągu każde dwa piksele o numerach i i i+1 są sąsiadujące.
    
    Opis wejścia
    Program ma wczytywać dane ze standardowego wejścia.
    
    W pierwszej linii wejścia znajduje się jedna liczba 0 < Z <= 100 oznaczająca ilość zestawów danych do przetworzenia.
    W kolejnych liniach znajdują się zestawy danych.
    Pierwsza linia zestawu danych zawiera dwie liczby naturalne oddzielone pojedynczą spacją: H W M (w tej kolejności), 1 < H, W <= 107, 0 < M <= 106 oznaczające odpowiednio wysokość i długość wyświetlacza LED oraz ilość złych pikseli.
    W kolejnych M liniach znajdują się odczyty złych pikseli zwrócone przez skaner Ady. Każdy opis złego piksela skada się z dwóch liczb x y oddzielonych pojedynczą spacją, które oznaczają jego pozycję w matrycy, odpowiednio w poziomie (x) i pionie (y). Dodatkowo mamy: 0 <= x < W, 0 <= y < H,
    Uwaga: lewy dolny róg ekranu ma spółrzedne (0,0), a prawy górny (H-1, W-1).
    Opis wyjścia
    Program ma wypisać rozwiązanie na standardowe wyjście. program ma wypisać dokładnie Z zestawów odpowiedzi, po jednej dla każdego zestawu danych.
    
    Dla i-tego zestawu danych program ma wypisać w pierwszej linii tego zestawu jedną liczbę Ci - ilość spójnych fragmentów złych pikseli na ekranie. W kolejnych Ci liniach ma wypisać po jednym pikselu z każdego spójnego fragmentu, przy czym wymagamy, aby był to piksel o najmniejszej współrzędnej y spośród wszystkich pikseli tego fragmentu. Jeśli jest kilka takich pikseli, należy wypisać ten z nich, który ma najmniejszą współrzedną x.
    
    Składowe mają być wypisywane w kolejności takiej w jakiej wybierani są reprezentanci, tzn. najpierw przedstawiciel o najmnoejszej współrzędnej y, a jeśli jest kilka takich przedstawicieli, należy wypisać ten z nich, który ma najmniejszą współrzedną x.
    
    Obostrzenia
    Plagiaty będą rozpatrywane na koniec semestru.
    Przykładowe wejście
    Dla danych:
    
    1
    10 10 22
    2 2
    2 3
    2 4
    2 5
    2 6
    2 7
    3 2
    4 2
    5 2
    6 2
    7 2
    7 3
    7 4
    7 5
    7 6
    7 7
    3 7
    4 7
    5 7
    6 7
    4 4
    5 5
    Poprawnym wyjściem jest:
    
    3
    2 2
    4 4
    5 5
    Matryca z przykładowego testu (o - dobre piksele, x - złe piksele):
    
    oooooooooo
    oooooooooo
    ooxxxxxxoo
    ooxooooxoo
    ooxooxoxoo
    ooxoxooxoo
    ooxooooxoo
    ooxxxxxxoo
    oooooooooo
    oooooooooo

---
+++ BACA A6.cpp
@@ -0,0 +1,75 @@
+﻿//Karol Bargiel
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <map>
+using namespace std;
+
+const int dx[] = { 1, 0, -1, 0 };
+const int dy[] = { 0, 1, 0, -1 };
+vector<pair<int, int>> groups;
+
+
+bool comparePixels(const pair<int,int>& p1, const pair<int, int>& p2) {
+    if (p1.second == p2.second) {
+        return p1.first < p2.first;
+    }
+    return p1.second < p2.second;
+}
+
+void DFS( pair<int,int>& badPixel, map<pair<int, int>, bool>& visitedPixels) {
+    visitedPixels[badPixel] = true;
+    int cx = badPixel.first;
+    int cy = badPixel.second;
+
+    for (int i = 0; i < 4; i++) {
+        int nx = cx + dx[i];
+        int ny = cy + dy[i];
+        auto neighbor = make_pair(nx, ny);
+
+        auto it = visitedPixels.find(neighbor);
+
+        if (it != visitedPixels.end() && !it->second) {
+            visitedPixels[neighbor] = true;
+            DFS(neighbor, visitedPixels);
+        }
+    }
+}
+
+int main() {
+    int Z;
+    cin >> Z;
+
+    while (Z-- > 0) {
+        int H, W, M;
+        cin >> H >> W >> M;
+
+        map<pair<int, int>, bool> visitedPixels;
+        vector < pair<int, int> > pairs(M);
+        for (int i = 0; i < M; ++i) {
+            int x, y;
+            cin >> x >> y;
+            pairs[i] = make_pair(x, y);
+        }
+        sort(pairs.begin(), pairs.end(), comparePixels);
+        for (int i = 0; i < pairs.size(); i++)
+        {
+            visitedPixels[pairs[i]] = false;
+        }
+        for (int i = 0; i < M; i++) {
+            if (!visitedPixels[pairs[i]]) {
+                DFS( pairs[i], visitedPixels);
+                groups.push_back({ pairs[i].first, pairs[i].second });
+            }
+        }
+
+        cout << groups.size() << endl;
+
+        for (const auto& component : groups) {
+            cout << component.first << " " << component.second << endl;
+        }
+        groups.clear();
+    }
+
+    return 0;
+}
